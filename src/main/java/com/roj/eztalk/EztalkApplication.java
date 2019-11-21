42d5b327e19257f8369344de408f679ce65d9e09
Choi Lam Wong <julian.wcl@outlook.com>
2019-11-21 18:11:01 +0000

    Initial Commit

---
+++ src/main/java/com/roj/eztalk/EztalkApplication.java
@@ -0,0 +1,13 @@
+package com.roj.eztalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EztalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EztalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roj/eztalk/EztalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roj.eztalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EztalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

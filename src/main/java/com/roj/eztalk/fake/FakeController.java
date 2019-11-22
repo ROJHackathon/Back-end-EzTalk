a6502826b76fc54190afd8436d61fa6601b27c29
Choi Lam Wong <julian.wcl@outlook.com>
2019-11-22 00:30:14 +0000

    change List.of to List.add

--- src/main/java/com/roj/eztalk/fake/FakeController.java
+++ src/main/java/com/roj/eztalk/fake/FakeController.java
@@ -1,7 +1,8 @@
 package com.roj.eztalk.fake;
 
 import com.roj.eztalk.*;
-import java.util.List;;
+import java.util.List;
+import java.util.ArrayList;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,13 +22,13 @@ public class FakeController {
   }
   @RequestMapping("/request-feed")
   public List<Material> fakeFeed() {
-      return List.of(
-          new Material("French I", "an intro to French", "French", "ROJFake", "www.fake.com", "false"),
-          new Material("French II", "an intro to French", "French", "ROJFake", "www.fake.com", "false"),
-          new Material("In a French Restaurant", "common used sentence in a French restaurant", "French", "ROJFake", "www.fake.com", "false"),
-          new Material("French Travel Advice", "advice for tourists", "French", "ROJFake", "www.fake.com", "false"),
-          new Material("French food", "flash-card of various french dished", "French", "ROJFake", "www.fake.com", "true")
-      );
+    ArrayList<Material> ret = new ArrayList<>();
+    ret.add(new Material("French I", "an intro to French", "French", "ROJFake", "www.fake.com", "false"));
+    ret.add(new Material("French II", "an intro to French", "French", "ROJFake", "www.fake.com", "false"));
+    ret.add(new Material("In a French Restaurant", "common used sentence in a French restaurant", "French", "ROJFake", "www.fake.com", "false"));
+    ret.add(new Material("French Travel Advice", "advice for tourists", "French", "ROJFake", "www.fake.com", "false"));
+    ret.add(new Material("French food", "flash-card of various french dished", "French", "ROJFake", "www.fake.com", "true"));
+    return ret;
   }
   
 

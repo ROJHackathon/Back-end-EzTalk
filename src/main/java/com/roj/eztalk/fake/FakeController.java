7609bedb96ff76ef8396c47b5dc621089b322ee0
Choi Lam Wong <julian.wcl@outlook.com>
2019-11-23 13:00:40 +0000

    fixed CORS error

--- src/main/java/com/roj/eztalk/fake/FakeController.java
+++ src/main/java/com/roj/eztalk/fake/FakeController.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 @RequestMapping("/api-fake/")
 public class FakeController {
